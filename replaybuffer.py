22924209fdd984afc89a465ce541b6557955794c
SulRash <sultan.m.rashed@gmail.com>
2024-05-05 22:18:08 +0000

    Added a fix for GAE using numpy_to_tensor

--- replaybuffer.py
+++ replaybuffer.py
@@ -5,7 +5,7 @@ import numpy as np
 class ReplayBuffer:
     def __init__(self, args):
         self.s = torch.zeros((args.batch_size, args.state_dim)).to('cuda')
-        self.a = torch.zeros((args.batch_size, 1)).to('cuda')
+        self.a = torch.zeros((args.batch_size, 1), dtype=torch.long).to('cuda')
         self.a_logprob = torch.zeros((args.batch_size, 1)).to('cuda')
         self.r = np.zeros((args.batch_size, 1))
         self.s_ = torch.zeros((args.batch_size, args.state_dim)).to('cuda')
@@ -24,12 +24,12 @@ class ReplayBuffer:
         self.count += 1
 
     def numpy_to_tensor(self):
-        s = torch.tensor(self.s, dtype=torch.float)
-        a = torch.tensor(self.a, dtype=torch.long)  # In discrete action space, 'a' needs to be torch.long
+        # s = torch.tensor(self.s, dtype=torch.float)
+        # a = torch.tensor(self.a, dtype=torch.long)  # In discrete action space, 'a' needs to be torch.long
         a_logprob = torch.tensor(self.a_logprob, dtype=torch.float)
-        r = torch.tensor(self.r, dtype=torch.float)
-        s_ = torch.tensor(self.s_, dtype=torch.float)
-        dw = torch.tensor(self.dw, dtype=torch.float)
-        done = torch.tensor(self.done, dtype=torch.float)
+        # r = torch.tensor(self.r, dtype=torch.float)
+        # s_ = torch.tensor(self.s_, dtype=torch.float)
+        # dw = torch.tensor(self.dw, dtype=torch.float)
+        # done = torch.tensor(self.done, dtype=torch.float)
 
-        return s, a, a_logprob, r, s_, dw, done
+        return self.s, self.a, a_logprob, self.r, self.s_, self.dw, self.done
