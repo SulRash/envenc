cc2e49323b5f22d585ba6b07edfe656eea1f5d8c
SulRash <sultan.m.rashed@gmail.com>
2024-05-08 10:42:27 +0000

    Removed redundant files and added vlm

--- main_cleanrl.py
+++ main_cleanrl.py
@@ -21,6 +21,16 @@ from stable_baselines3.common.atari_wrappers import (  # isort:skip
     NoopResetEnv,
 )
 
+from tinyllava.model.builder import load_pretrained_model
+from tinyllava.mm_utils import get_model_name_from_path
+
+model_path = "bczhou/TinyLLaVA-2.0B"
+
+tokenizer, model, image_processor, context_len = load_pretrained_model(
+    model_path=model_path,
+    model_base=None,
+    model_name=get_model_name_from_path(model_path)
+)
 
 @dataclass
 class Args:
