b6a1a7ac785c837d8196236b2208f16edb082666
SulRash <sultan.m.rashed@gmail.com>
2024-05-08 13:48:00 +0000

    Removed quantization due to errors

--- main.py
+++ main.py
@@ -33,8 +33,7 @@ model_path = "bczhou/TinyLLaVA-2.0B"
 tokenizer, model, image_processor, context_len = load_pretrained_model(
     model_path=model_path,
     model_base=None,
-    model_name=get_model_name_from_path(model_path),
-    load_4bit=True
+    model_name=get_model_name_from_path(model_path)
 )
 
 model = torch.compile(model)
