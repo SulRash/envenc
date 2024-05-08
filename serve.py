de0ed858e9d6d7261f37343293e445749a2fd861
SulRash <sultan.m.rashed@gmail.com>
2024-05-08 13:37:14 +0000

    Added some torch backend code and commented out useless stuff

--- serve.py
+++ serve.py
@@ -21,65 +21,6 @@ def load_image(image_file):
         image = Image.open(image_file).convert('RGB')
     return image
 
-def infer(tokenizer, model, image_processor, inp, image_file):
-    disable_torch_init()
-
-    max_new_tokens = 256
-    
-    debug = False
-    temperature = 0
-    conv_mode = 'phi'
-    conv = conv_templates[conv_mode].copy()
-
-    if image_file is not None:
-        image = load_image(image_file)
-        image_tensor = process_images([image], image_processor, model.config)
-        if type(image_tensor) is list:
-            image_tensor = [image.to(model.device, dtype=torch.float16) for image in image_tensor]
-        else:
-            image_tensor = image_tensor.to(model.device, dtype=torch.float16)
-    else:
-        image = None
-    if image is not None:
-        if model.config.mm_use_im_start_end:
-            inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
-        else:
-            inp = DEFAULT_IMAGE_TOKEN + '\n' + inp
-        conv.append_message(conv.roles[0], inp)
-        image = None
-    else:
-        # later messages
-        conv.append_message(conv.roles[0], inp)
-        image_tensor = None
-    conv.append_message(conv.roles[1], None)
-    prompt = conv.get_prompt()
-
-    input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(model.device)
-    stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
-    keywords = [stop_str]
-    stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
-    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
-
-    with torch.inference_mode():
-        output_ids = model.generate(
-            input_ids,
-            images=image_tensor,
-            do_sample=True if temperature > 0 else False,
-            temperature=temperature,
-            max_new_tokens=max_new_tokens,
-            streamer=streamer,
-            use_cache=True,
-            stopping_criteria=[stopping_criteria]
-        )
-
-    outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
-    conv.messages[-1][-1] = outputs
-
-    if debug:
-        print("\n", {"prompt": prompt, "outputs": outputs}, "\n")
-    
-    return outputs, output_ids
-
 def infer_no_lm(tokenizer, model, image_processor, image_array):
     disable_torch_init()
     
@@ -129,7 +70,7 @@ def infer_no_lm(tokenizer, model, image_processor, image_array):
     keywords = [stop_str]
     stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
 
-    with torch.no_grad():
+    with torch.backends.cuda.sdp_kernel(enable_flash=True, enable_math=False, enable_mem_efficient=False) and torch.no_grad():
         out = model(
             input_ids,
             images=image_tensor,
@@ -138,3 +79,62 @@ def infer_no_lm(tokenizer, model, image_processor, image_array):
         merged_out = out.hidden_states[0][0].mean(dim=0)
     
     return merged_out
+
+# def infer(tokenizer, model, image_processor, inp, image_file):
+#     disable_torch_init()
+
+#     max_new_tokens = 256
+    
+#     debug = False
+#     temperature = 0
+#     conv_mode = 'phi'
+#     conv = conv_templates[conv_mode].copy()
+
+#     if image_file is not None:
+#         image = load_image(image_file)
+#         image_tensor = process_images([image], image_processor, model.config)
+#         if type(image_tensor) is list:
+#             image_tensor = [image.to(model.device, dtype=torch.float16) for image in image_tensor]
+#         else:
+#             image_tensor = image_tensor.to(model.device, dtype=torch.float16)
+#     else:
+#         image = None
+#     if image is not None:
+#         if model.config.mm_use_im_start_end:
+#             inp = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + inp
+#         else:
+#             inp = DEFAULT_IMAGE_TOKEN + '\n' + inp
+#         conv.append_message(conv.roles[0], inp)
+#         image = None
+#     else:
+#         # later messages
+#         conv.append_message(conv.roles[0], inp)
+#         image_tensor = None
+#     conv.append_message(conv.roles[1], None)
+#     prompt = conv.get_prompt()
+
+#     input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(model.device)
+#     stop_str = conv.sep if conv.sep_style != SeparatorStyle.TWO else conv.sep2
+#     keywords = [stop_str]
+#     stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+#     streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+#     with torch.inference_mode():
+#         output_ids = model.generate(
+#             input_ids,
+#             images=image_tensor,
+#             do_sample=True if temperature > 0 else False,
+#             temperature=temperature,
+#             max_new_tokens=max_new_tokens,
+#             streamer=streamer,
+#             use_cache=True,
+#             stopping_criteria=[stopping_criteria]
+#         )
+
+#     outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
+#     conv.messages[-1][-1] = outputs
+
+#     if debug:
+#         print("\n", {"prompt": prompt, "outputs": outputs}, "\n")
+    
+#     return outputs, output_ids
