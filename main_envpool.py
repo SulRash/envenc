874603fde394328f60bf699d645d2495d86f2d2c
SulRash <sultan.m.rashed@gmail.com>
2024-05-14 19:45:28 +0000

    updated wandb project name

--- main_envpool.py
+++ main_envpool.py
@@ -36,7 +36,7 @@ class Args:
     """if toggled, cuda will be enabled by default"""
     track: bool = False
     """if toggled, this experiment will be tracked with Weights and Biases"""
-    wandb_project_name: str = "cleanRL"
+    wandb_project_name: str = "envenc"
     """the wandb's project name"""
     wandb_entity: str = None
     """the entity (team) of wandb's project"""
