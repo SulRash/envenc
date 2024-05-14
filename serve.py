d6b65e6456a80b7c0b95aa2f266e20ce1a01da4e
SulRash <sultan.m.rashed@gmail.com>
2024-05-14 13:39:29 +0000

    Cleaned up serving file and removed qwen-vl integration for now

--- serve.py
+++ serve.py
@@ -10,16 +10,7 @@ from tinyllava.mm_utils import process_images, tokenizer_image_token
 from PIL import Image
 from io import BytesIO
 
-
-def load_image(image_file):
-    if image_file.startswith('http://') or image_file.startswith('https://'):
-        response = requests.get(image_file)
-        image = Image.open(BytesIO(response.content)).convert('RGB')
-    else:
-        image = Image.open(image_file).convert('RGB')
-    return image
-
-def infer_no_lm(tokenizer, model, image_processor, image_arrays):
+def infer_tinyllava(tokenizer, model, image_processor, image_arrays):
     disable_torch_init()
     
     input_ids = torch.zeros((image_arrays.shape[0], 49), dtype=torch.long).to(model.device)
@@ -28,8 +19,8 @@ def infer_no_lm(tokenizer, model, image_processor, image_arrays):
     inp = 'Describe the image in detail.'
 
     conv = Conversation(
-        system='A chat between a curious user and an artificial intelligence assistant. '
-           'The assistant gives helpful, detailed, and polite answers to the user's questions.',
+        system="A chat between a curious user and an artificial intelligence assistant. "
+           "The assistant gives helpful, detailed, and polite answers to the user's questions.",
         roles=('USER', 'ASSISTANT'),
         version='phi',
         messages=[],
@@ -96,66 +87,6 @@ def infer_no_lm(tokenizer, model, image_processor, image_arrays):
     return merged_out
 
 
-# Qwen-VL implementation for VLM
-# Struggling with the performance issue of saving picture -> getting path -> querying -> deleting image loop.
-# Switching over to minigpt-4
-# def serve_qwen(
-#         tokenizer,
-#         model,
-#         image_arrays,
-#         device: str = 'cuda',
-#         system: str = 'You are a helpful AI game guide, assist the user with any queries they ask helpfully and intelligently.',
-#         decode: bool = False
-#     ):
-
-#     generation_config = generation_config if generation_config is not None else model.generation_config
-#     print(generation_config)
-
-#     history = []
-#     stop_words_ids = []
-
-#     query = tokenizer.from_list_format([
-#         {'image': 'https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg'},
-#         {'text': '这是什么'},
-#     ])
-
-#     max_window_size = generation_config.max_window_size
-#     raw_text, context_tokens = make_context(
-#         tokenizer,
-#         query,
-#         history=history,
-#         system=system,
-#         max_window_size=max_window_size,
-#         chat_format=generation_config.chat_format,
-#     )
-
-#     stop_words_ids.extend(get_stop_words_ids(
-#         generation_config.chat_format, tokenizer
-#     ))
-#     input_ids = torch.tensor([context_tokens]).to(device)
-#     outputs = model.generate(
-#                 input_ids,
-#                 stop_words_ids=stop_words_ids,
-#                 return_dict_in_generate=True,
-#                 generation_config=generation_config,
-#             )
-
-#     if decode:
-#         response = decode_tokens(
-#             outputs[0],
-#             tokenizer,
-#             raw_text_len=len(raw_text),
-#             context_length=len(context_tokens),
-#             chat_format=generation_config.chat_format,
-#             verbose=False,
-#             errors='replace'
-#         )
-#         print(response)
-
-#     print(outputs)
-
-
-
 def infer_idefics(image_arrays, device = 'cuda', **kwargs):
 
     processor = kwargs['processor']
@@ -191,48 +122,3 @@ def infer_idefics(image_arrays, device = 'cuda', **kwargs):
         generated_texts = processor.batch_decode([generated_ids], skip_special_tokens=True)
 
     print(generated_texts)
-    # ['User: What do we see in this image? \nAssistant: In this image, we can see the city of New York, and more specifically the Statue of Liberty. \nUser: And how about this image? \nAssistant: In this image we can see buildings, trees, lights, water and sky.']
-
-def model_loader(vlm: str = 'idefics', load_4bit: bool = False, device: str = "cuda"):
-    vlms = ['tinyllava', 'idefics']
-
-    if vlm == 'tinyllava':
-        from tinyllava.model.builder import load_pretrained_model
-        from tinyllava.mm_utils import get_model_name_from_path
-
-        model_path = "bczhou/TinyLLaVA-2.0B"
-
-        if load_4bit:
-            processor, model, vision_tower, _ = load_pretrained_model(
-                model_path=model_path,
-                model_base=None,
-                model_name=get_model_name_from_path(model_path),
-                load_4bit=True
-            )
-            image_processor = vision_tower.image_processor
-
-        else:
-            processor, model, vision_tower, _ = load_pretrained_model(
-                model_path=model_path,
-                model_base=None,
-                model_name=get_model_name_from_path(model_path),
-                load_4bit=False
-            )
-            image_processor = torch.compile(vision_tower).image_processor
-            model = torch.compile(model)
-        
-    elif vlm == 'idefics':
-        from transformers import AutoProcessor, AutoModelForVision2Seq
-
-        processor = AutoProcessor.from_pretrained('HuggingFaceM4/idefics2-8b')
-        model = AutoModelForVision2Seq.from_pretrained(
-            'HuggingFaceM4/idefics2-8b',
-            torch_dtype=torch.float16,
-            _attn_implementation="flash_attention_2"
-        ).to(device)
-        image_processor = None
-        model = torch.compile(model)
-    
-    else:
-        raise(f"Invalid VLM choice! Possible options: {vlms}")
-
