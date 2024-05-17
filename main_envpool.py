26d5813b6791447e12d1697da6681bdd77a9e212
SulRash <sultan.m.rashed@gmail.com>
2024-05-17 18:27:52 +0000

    Updated run with new defaults and scripts that seem to go better

--- main_envpool.py
+++ main_envpool.py
@@ -231,31 +231,19 @@ class AgentCNN(nn.Module):
 class AgentMLP(nn.Module):
     def __init__(self, envs):
         super().__init__()
-        # self.actor = nn.Sequential(
-        #     layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 12800)),
-        #     nn.Tanh(),
-        #     layer_init(nn.Linear(12800, 5184)),
-        #     nn.Tanh(),
-        #     layer_init(nn.Linear(5184, 3136)),
-        #     nn.Tanh(),
-        #     layer_init(nn.Linear(3136, 512)),
-        #     nn.Tanh(),
-        #     layer_init(nn.Linear(512, envs.single_action_space.n), std=0.01)
-        # )
+        self.actor = nn.Sequential(
+            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 1024)),
+            nn.Tanh(),
+            layer_init(nn.Linear(1024, envs.single_action_space.n), std=0.01)
+        )
 
-        # self.critic = nn.Sequential(
-        #     layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 12800)),
-        #     nn.Tanh(),
-        #     layer_init(nn.Linear(12800, 5184)),
-        #     nn.Tanh(),
-        #     layer_init(nn.Linear(5184, 3136)),
-        #     nn.Tanh(),
-        #     layer_init(nn.Linear(3136, 512)),
-        #     nn.Tanh(),
-        #     layer_init(nn.Linear(512, 1), std=1)
-        # )
-        self.actor = layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), envs.single_action_space.n), std=0.01)
-        self.critic = layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 1), std=1)
+        self.critic = nn.Sequential(
+            layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 1024)),
+            nn.Tanh(),
+            layer_init(nn.Linear(1024, 1), std=1)
+        )
+        # self.actor = layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), envs.single_action_space.n), std=0.01)
+        # self.critic = layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(), 1), std=1)
 
     def get_value(self, x):
         return self.critic(x)
